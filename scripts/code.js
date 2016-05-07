3c5918fe3e35c72b1aa1584932641e754ab88966
Jason Brooks <jason.brooks@yale.edu>
2016-05-06 21:35:56 -0400

    Adeded second page gallery

--- scripts/code.js
+++ scripts/code.js
@@ -3,18 +3,6 @@
 /*::::::::::::::::::::::::::::::::::::::::::::::::*/
 
 $(document).ready(function() {
-	$('.description').on('click', function() {
-		if ($(this).hasClass("expanded")) {
-			$(this).animate({height: 67}, 200);
-			$('.main-image-caption').animate({height: 67}, 0);
-			$(this).removeClass("expanded");
-			$("#elipse-image").fadeIn();
-		} else {
-			$(this).addClass("expanded");
-			$("#elipse-image").hide();
-			$(this).animate({height: 125}, 200);
-			$('.main-image-caption').animate({height: 125}, 0);
-		}
-	})
+
 
 });
--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+	$('.description').on('click', function() {
+		if ($(this).hasClass("expanded")) {
+			$(this).animate({height: 67}, 200);
+			$('.main-image-caption').animate({height: 67}, 0);
+			$(this).removeClass("expanded");
+			$("#elipse-image").fadeIn();
+		} else {
+			$(this).addClass("expanded");
+			$("#elipse-image").hide();
+			$(this).animate({height: 125}, 200);
+			$('.main-image-caption').animate({height: 125}, 0);
+		}
+	});
 	$("#bottom-image-gallery-scroller").scroll(function() {
 		$("#left-gallery-scroll-arrow").css('left', 15+$(this).scrollLeft());
 		$("#right-gallery-scroll-arrow").css('left', 595+$(this).scrollLeft());
@@ -15,8 +28,9 @@ $(document).ready(function() {
 		}
 	});
 
-	$("#bottom-image-gallery-scroller > img").on("click", function() {
-		console.log("An image has been clicked");
+	$("#bottom-image-gallery-scroller > img, #bottom-image-gallery-scroller > * > img").on("click", function() {
 		$("#main-image").attr("src", $(this).attr('src'));
-	})
+		$("#bottom-image-gallery-scroller").find("img").removeClass("image-selected");
+		$(this).addClass("image-selected");
+	});
 });
